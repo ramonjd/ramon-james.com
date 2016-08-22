678bf350e366f86c56ec250f75888ec756862fe7
ramonjd <ramonjd@gmail.com>
2016-08-22 22:11:56 +0100

    @ramonjd travis test

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x # Show the output of the following commands (useful for debugging)
 
-openssl aes-256-cbc -K $encrypted_33d7cde45114_key -iv $encrypted_33d7cde45114_iv -in deploy-key.enc -out ~\/.ssh/deploy-key -d
+openssl aes-256-cbc -K $encrypted_33d7cde45114_key -iv $encrypted_33d7cde45114_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc # Don't need it anymore
-chmod 600 ~/.ssh/deploy-key
+chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
