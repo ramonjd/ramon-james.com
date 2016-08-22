4dd585f8c0d7f279c5a2cef319235f98201e136e
ramonjd <ramonjd@gmail.com>
2016-08-22 22:21:27 +0100

    @ramonjd travis test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,15 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     mkdir _site
-    cp dist api assets index.js server.js /_site
+    cp dist  _site/
+    cp api _site/
+    cp assets _site/
+    cp server.js _site/
+    cp index.js _site/
+
     cd _site
     git init
-    git remote add deploy "deploy@ramon-james.com:/var/www/ramon-james.com"
+    git remote add deploy "root@ramon-james.com:/var/www/ramon-james.com"
     git config user.name "Travis CI"
     git config user.email "ramonjd+travisCI@gmail.com"
     git push --force deploy master
