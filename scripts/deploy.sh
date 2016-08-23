9cdf95ab2dc0cc8e70a791befa667fc5edc8352c
ramonjd <ramonjd@gmail.com>
2016-08-23 09:11:36 +0100

    @ramonjd travis deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-    # mkdir _site
-    # cp dist  _site/
-    # cp api _site/
-    # cp assets _site/
-    # cp server.js _site/
-    # cp index.js _site/
-    #
-    # cd _site
-    # git init
-    # git remote add deploy "deploy@ramon-james.com:/var/www/ramon-james.com"
-    # git config user.name "Travis CI"
-    # git config user.email "ramonjd+travisCI@gmail.com"
-    # git push --force deploy master
     ssh deploy@ramon-james.com "cd /var/www/ramon-james.com && git pull"
 else
     echo "Not deploying, since this branch isn't master."
